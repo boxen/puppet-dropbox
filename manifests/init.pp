aa1ac1e7a2c25666b05c92e1f81811219577954d
Kevin Suttle <kevin@kevinsuttle.com>
2013-03-12 16:28:46 -0300

    Update Dropbox to 2.0.0 release
    Newest stable version released today.

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,6 +6,6 @@
 class dropbox {
   package { 'Dropbox':
     provider => 'appdmg',
-    source   => 'https://d1ilhw0800yew8.cloudfront.net/client/Dropbox%201.6.17.dmg'
+    source   => 'https://d1ilhw0800yew8.cloudfront.net/client/Dropbox%202.0.0.dmg'
   }
 }
