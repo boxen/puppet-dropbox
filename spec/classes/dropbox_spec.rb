73e3ac4b99a1a65858ea7bbe8bf7a83f19487298
ddebree <ddebree@gmail.com>
2013-10-30 19:50:24 +0200

    Fixed redirect link

--- spec/classes/dropbox_spec.rb
+++ spec/classes/dropbox_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe 'dropbox' do
   it do
     should contain_package('Dropbox').with({
-      :source   => 'https://www.dropbox.com/download?plat=mac',
+      :source   => 'https://d1ilhw0800yew8.cloudfront.net/client/Dropbox%202.4.5.dmg',
       :provider => 'appdmg'
     })
   end
