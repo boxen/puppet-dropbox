00068322b5ff26b57c34dd28c477a7ac24605df3
Takashi Yoshida <takashi@takashiyoshida.org>
2014-01-19 00:54:10 +0800

    Bump version to 2.6.2

--- spec/classes/dropbox_spec.rb
+++ spec/classes/dropbox_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe 'dropbox' do
   it do
     should contain_package('Dropbox').with({
-      :source   => 'https://d1ilhw0800yew8.cloudfront.net/client/Dropbox%202.4.6.dmg',
+      :source   => 'https://d1ilhw0800yew8.cloudfront.net/client/Dropbox%202.6.2.dmg',
       :provider => 'appdmg'
     })
   end
