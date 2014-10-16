33c500a6d11f2a325d89e5b1481728cfcf019f7d
Takashi Yoshida <takashi@takashiyoshida.org>
2014-10-16 20:50:24 +0800

    Remove 'ensure' as it is not necessary to include

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,7 +7,7 @@ class dropbox (
   $version = '2.10.41',
 ){
   package { 'Dropbox':
-    ensure   => installed,
+#    ensure   => installed,
     provider => 'appdmg',
     source   => "https://d1ilhw0800yew8.cloudfront.net/client/Dropbox%20${version}.dmg",
   }
